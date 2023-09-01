8c0ce883e25663fe180865873c0b65c670adc943
Zakaryae Ennaciri <ezakaryae7@gmail.com>
2023-09-01 11:59:40 +0100

    1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,27 @@ import { useReactToPrint } from 'react-to-print';
 import fullLogo from '/Full.png'
 
 function App() {
-  <h1>Hello world :)</h1>
+  // FOR MANIFEST APP
+  let deferredPrompt;
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+  });
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  }
+
+  return(
+    <h1>Hello world</h1>
+  )
 }
 export default App;
